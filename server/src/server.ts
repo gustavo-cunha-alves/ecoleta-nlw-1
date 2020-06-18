137cea43f979122d747ea5cbc8fdf0159615dbb3
Gustavo Cunha <alves.gustavocunha@gmail.com>
2020-06-17 23:43:55 -0300

    second class

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
-import express, { response } from 'express'
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (req, res) => {
-    console.log('listagem de usuarios')
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-    response.send('Hello world')
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333, () => {
     console.log('running on port 3333')
